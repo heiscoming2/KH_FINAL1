dfc9f17be738d2b1062278d7927710fd2aaa5baf
heiscoming2 <heiscoming@kakao.com>
2021-04-07 00:09:02 +0900

    first upload

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/FinalProject/js/studydetail.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#summernote').summernote({
+      placeholder: '댓글을 작성해주세요.',
+      height:150
+    });
+  });
---
+++ FinalProject/WebContent/js/studyinsert.js
@@ -0,0 +1,7 @@
+  /* 썸머노트 jqeury*/
+  $(document).ready(function() {
+    $('#summernote').summernote({
+      placeholder: '상세 내용을 입력해주세요.',
+      height:500
+    });
+  });
