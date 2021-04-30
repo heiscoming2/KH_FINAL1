f586a3030ca8faea7c26deba62fe05b841dd7fae
hyojun9292 <hyojun9292@naver.com>
2021-04-30 13:46:17 +0900

    BOARD ADD BD_REPLYCOUNT

---
+++ src/main/java/com/itpro/model/dto/StudyListDto.java
@@ -0,0 +1,15 @@
+package com.itpro.model.dto;
+
+public class StudyListDto {
+	
+	private int bd_no;
+	private char st_status;
+	private String st_addr1;
+	private String st_addr2;
+	private int st_nowperson;
+	private int st_closeperson;
+	private String bd_title;
+	private int bd_replycount;
+	
+	
+}
