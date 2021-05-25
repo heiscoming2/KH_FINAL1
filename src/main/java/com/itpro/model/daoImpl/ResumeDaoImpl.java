21d24eba5a3d7ab15d22e64f66b9f3b88b9fec0f
dudrnsha90 <dudrnsha90@naver.com>
2021-05-25 15:26:25 +0900

    resume

--- src/main/java/com/itpro/model/daoImpl/ResumeDaoImpl.java
+++ src/main/java/com/itpro/model/daoImpl/ResumeDaoImpl.java
@@ -1,28 +1,30 @@
-package com.itpro.model.bizImpl;
+package com.itpro.model.daoImpl;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-import com.itpro.model.biz.MemberBiz;
-import com.itpro.model.biz.ResumeBiz;
 import com.itpro.model.dao.MemberDao;
 import com.itpro.model.dao.ResumeDao;
 import com.itpro.model.dto.member.MemberDto;
 import com.itpro.model.dto.member.ProfileDto;
 
-@Service
-public class MemberBizImpl implements ResumeBiz {
+@Repository
+public class ResumeDaoImpl implements ResumeDao {
 
 	@Autowired
-	private ResumeDao dao;
+	private SqlSessionTemplate sqlSession;
 
 	//이력서 등록
 	@Override
 	public MemberDto selectOne(int m_no) {
 		
-		return dao.selectOne(m_no);
+		return null;
 	}
 
 	
