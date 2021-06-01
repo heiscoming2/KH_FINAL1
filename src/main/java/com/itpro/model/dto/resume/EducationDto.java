ab8f7874656a03a966935fd6b9b254f4e169b3ef
guswl3391 <o_ovvz@naver.com>
2021-06-01 21:42:22 +0900

    education

---
+++ src/main/java/com/itpro/model/dto/resume/EducationDto.java
@@ -0,0 +1,21 @@
+package com.itpro.model.dto.resume;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EducationDto {
+
+	// 학력사항
+	private int m_no;
+	private int ed_no;
+	private String ed_date;
+	private String ed_schoolName;// 학교명
+	private String ed_graduation;// 졸업, 휴학, 중퇴, 재학
+	private String ed_major; // 전공
+	private String ed_school;// 대학원, 대학교, 고등학교
+
+}
