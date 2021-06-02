3f9914cd518cf0494c9ef9608e987d336d9809dd
dudrnsha90 <dudrnsha90@naver.com>
2021-06-02 09:10:37 +0900

    ddd

--- src/main/java/com/itpro/model/dao/NoteDao.java
+++ src/main/java/com/itpro/model/dao/NoteDao.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpSession;
 import com.itpro.model.dto.member.MemberDto;
 import com.itpro.model.dto.member.ProfileDto;
 import com.itpro.model.dto.note.NoteDto;
-import com.itpro.model.dto.resume.ResumeDto;
 
 public interface NoteDao {
 	String NAMESPACE = "Note.";
--- src/main/java/com/itpro/model/dto/resume/CareerDto.java
+++ src/main/java/com/itpro/model/dto/resume/CareerDto.java
@@ -1,10 +1,6 @@
 package com.itpro.model.dto.resume;
 
-<<<<<<< HEAD
 import java.util.Date;
-
-=======
->>>>>>> 0c3776293c843c98460ee7190f52575c6b021bfd
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,23 +10,13 @@ import lombok.ToString;
 @ToString
 public class CareerDto {
 
-<<<<<<< HEAD
 	// 멤버 기본 정보
 	private int m_no;// 회원번호
 
 	// 경력 및 교육 사항
-=======
-	// 경력 및 교육 사항
-	private int m_no;
->>>>>>> 0c3776293c843c98460ee7190f52575c6b021bfd
 	private int ca_no;
 	private String ca_title;
 	private String ca_content;
 	private String ca_start_date;
 	private String ca_end_date;
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 0c3776293c843c98460ee7190f52575c6b021bfd
 }
