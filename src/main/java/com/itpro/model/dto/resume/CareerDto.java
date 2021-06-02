33471a0e6995d7b2e9c4e3a40bb6ce128f0a0386
heiscoming2 <heiscoming@kakao.com>
2021-06-02 16:04:10 +0900

    ad

--- src/main/java/com/itpro/model/dto/resume/CareerDto.java
+++ src/main/java/com/itpro/model/dto/resume/CareerDto.java
@@ -12,13 +12,19 @@ public class CareerDto {
 
 
 	// 멤버 기본 정보
-	private int m_no;// 회원번호
+
 
 	// 경력 및 교육 사항
+
+
+	// 경력 및 교육 사항
+	private int m_no;
+
 	private int ca_no;
 	private String ca_title;
 	private String ca_content;
 	private String ca_start_date;
 	private String ca_end_date;
+
 
 }
